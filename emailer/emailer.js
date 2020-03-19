d842cc784e5fe4348e464b4fe67f6b65ac23f77b
pro-use <robertprouse@gmail.com>
2020-03-19 18:17:52 +0000

    add email templating

--- emailer/emailer.js
+++ emailer/emailer.js
@@ -1,4 +1,16 @@
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+viewEngine = handlebars.create({
+    defaultLayout: false
+});
+
+sut = nodemailerExpressHandlebars({
+    viewEngine: viewEngine,
+    viewPath: '../emails'
+});
 
 let transport = nodemailer.createTransport({
     host: 'smtp.mailtrap.io',
@@ -9,16 +21,21 @@ let transport = nodemailer.createTransport({
     }
 });
 
+transport.use('compile', sut);
+
 exports.emailResponse = (add, type) => {
     if (type === 'displayed') {
         var subject = "Arrivals Departures: Name now live";
         var msg = "Your submitted name is now on the board";
     }
     const message = {
-    from: 'info@arrivals-departures.com',
-    to: add,         // List of recipients
-    subject: subject, // Subject line
-    text: msg // Plain text body
+        from: 'info@arrivals-departures.com',
+        to: add,         // List of recipients
+        subject: subject, // Subject line
+        template: 'base_email',
+        context: {
+           name: msg
+        }
     };
     transport.sendMail(message, function(err, info) {
         if (err) {
