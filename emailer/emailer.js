54e9cb3823e944b86950604998e81243b7ba5e5c
pro-use <robertprouse@gmail.com>
2020-04-02 16:28:46 +0100

    send notification of name to be moderated

--- emailer/emailer.js
+++ emailer/emailer.js
@@ -47,6 +47,7 @@ exports.emailResponse = (add, type) => {
 };
 
 exports.emailModeration = (name) => {
+    console.log('Sending moderation message for '+name);
     var msg = 'Hi,<br><br> The name <strong>'+name+'</strong> has been entered and requires moderation. <br>' +
             'got to <a href="http://www.arrivalsanddepartures.net/moderation">'+
             'http://www.arrivalsanddepartures.net/moderation</a> to manually moderate.' +
