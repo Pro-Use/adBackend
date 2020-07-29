90837138092d45c15ca827c54584884632f3501c
pro-use <robertprouse@gmail.com>
2020-07-29 11:07:21 +0100

    Add logging to moderation

--- models/moderation.model.js
+++ models/moderation.model.js
@@ -30,25 +30,25 @@ exports.Moderate = (name, result) => {
                 var codes = results["Records"][0]["Results"].split(",");
                 codes.forEach(code => {
                    var prefix = code.substring(0,2).replace(/ /g, "");
-        //           console.log(code);
+                   console.log(code);//
                    if (prefix === "NE") {
                        errors += 1;
                    } else if (prefix === "NS") {
                        var success = parseInt(code.substring(2,4));
                        if (5 <= success && success <= 8){
-        //                   console.log("code is "+ success + " adding to census matches");
+                           console.log("code is "+ success + " adding to census matches");//
                            census_matches += 1;
                        }
                    }  
                 });
-        //        console.log("names: " + name_count);
-        //        console.log("total errors:" + errors);
-        //        console.log("total census matches:" + census_matches);
+                console.log("names: " + name_count);//
+                console.log("total errors:" + errors);//
+                console.log("total census matches:" + census_matches);//
                 if (errors > 0 ) {
                     moderated = 0;
                     result(moderated);
                 }else if (census_matches < name_count) {
-        //            console.log("moderation 2nd step");
+                    console.log("moderation 2nd step");//
         //          Name API Moderation - 2nd Pass
                     var options = {
                         uri: "http://api.nameapi.org/rest/v5.3/parser/personnameparser?apiKey=" + nameapi.KEY,
@@ -69,7 +69,7 @@ exports.Moderate = (name, result) => {
                       };
                     request(options, (error, response, body) => {
                         if (!error && response.statusCode === 200) {
-        //                  console.log(body.matches[0]);
+                          console.log(body.matches[0]);//
                           if (body.matches[0].likeliness < 0.5 || body.matches[0].confidence < 0.5 ) {
                               moderated = 0;
                               result(moderated);
